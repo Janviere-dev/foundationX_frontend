9f693696485c4ee0d0947b8e75443582cd32540e
Janviere-dev <j.munezero@alustudent.com>
2026-07-24 20:24:21 +0200

    Add bottom-tab MainNavigation shell for Home/Subjects/Progress/Profile

---
+++ lib/features/home/screens/main_navigation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:foundationx_frontend/core/theme/app_colors.dart';
+import 'package:foundationx_frontend/features/home/screens/home_screen.dart';
+import 'package:foundationx_frontend/features/subjects/screens/subjects_screen.dart';
+import 'package:foundationx_frontend/features/progress/screens/progress_screen.dart';
+import 'package:foundationx_frontend/features/profile/screens/profile_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    SubjectsScreen(),
+    ProgressScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+
+      bottomNavigationBar: SafeArea(
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+
+          type: BottomNavigationBarType.fixed,
+          elevation: 12,
+          selectedItemColor: AppColors.primary,
+          unselectedItemColor: const Color(0xFF8E98A8),
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book_outlined),
+              activeIcon: Icon(Icons.menu_book),
+              label: 'Subjects',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.leaderboard_outlined),
+              activeIcon: Icon(Icons.leaderboard),
+              label: 'Progress',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
