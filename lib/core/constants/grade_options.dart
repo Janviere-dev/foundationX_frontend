f5b282b454823b567032f7d838d68cccc510a077
Loicet <l.teta2@alustudent.com>
2026-07-29 10:57:24 +0200

    Add GradeOptions constant (Senior 1-6)

---
+++ lib/core/constants/grade_options.dart
@@ -0,0 +1,12 @@
+class GradeOptions {
+  GradeOptions._();
+
+  static const List<String> all = [
+    'Senior 1',
+    'Senior 2',
+    'Senior 3',
+    'Senior 4',
+    'Senior 5',
+    'Senior 6',
+  ];
+}
