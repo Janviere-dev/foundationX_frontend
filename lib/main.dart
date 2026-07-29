121ca92ef698ecb27cf6c4e3a5cd3ec495062f9d
Loicet <l.teta2@alustudent.com>
2026-07-29 11:41:43 +0200

    Make routing auth-aware; initialize Firebase/GoogleSignIn at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,50 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:foundationx_frontend/core/providers/app_providers.dart';
 import 'package:foundationx_frontend/core/providers/theme_provider.dart';
 import 'package:foundationx_frontend/core/theme/app_theme.dart';
+import 'package:foundationx_frontend/features/auth/providers/auth_provider.dart';
 import 'package:foundationx_frontend/router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'firebase_options.dart';
 
-void main() async {
+
+const _googleWebClientId =
+    '902759665628-edogl9prngd6qorn8aeraodke4ubgast.apps.googleusercontent.com';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await GoogleSignIn.instance.initialize(
+    clientId: kIsWeb ? _googleWebClientId : null,
+    serverClientId: kIsWeb ? null : _googleWebClientId,
+  );
   final prefs = await SharedPreferences.getInstance();
 
   runApp(
     MultiProvider(
-      providers: AppProviders.providers(prefs),
+      providers: [
+        ...AppProviders.providers(prefs),
+      ],
       child: const MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  late final GoRouter _router = buildRouter(context.read<AuthProvider>());
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +56,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: themeMode,
-      routerConfig: appRouter,
+      routerConfig: _router,
     );
   }
 }
